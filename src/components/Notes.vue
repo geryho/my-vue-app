464b1975edaf0d70640efd60ddc28fea159153cc
Gery <geryho88@gmail.com>
2025-07-24 15:18:17 +0700

    modified files index.html, notes.html, notes.vue

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -2,6 +2,32 @@
 
 <template>
   <main class="h-screen w-screen">
+    <!-- <div
+      class="overlay absolute w-[100%] h-[100%] bg-black/70 z-10 flex justify-center items-center"
+    >
+       <div
+        class="modal w-[600px] bg-white rounded-[10px] p-[30px] relative flex flex-col items-center"
+      >
+        <textarea
+          name="note"
+          id="note"
+          cols="30"
+          rows="0"
+          class="border-1 h-[150px] w-[550px]"
+        ></textarea>
+        <button
+          class="px-[10] py-[20] w-[40%] items-center bg-violet-600 border-none text-white cursor-pointer mt-[15px]"
+        >
+          Add Note
+        </button>
+        <button
+          class="px-[10] py-[20] w-[40%] bg-red-600 border-none text-white cursor-pointer mt-[7px]"
+        >
+          Close
+        </button>
+      </div>
+    </div>-->
+
     <div class="container max-w-[1000px] p-[10px] mx-auto my-0">
       <header class="flex justify-between items-center">
         <h1 class="font-bold mb-[25px] text-[75px]">Notes</h1>
@@ -11,7 +37,16 @@
           +
         </button>
       </header>
-      <div class="cards-container">
+      <div class="cards-container flex flex-wrap">
+        <div
+          class="card w-[225px] h-[225px] bg-amber-300 p-[10px] rounded-[15px] flex flex-col justify-between mr-[20px] mb-[20px]"
+        >
+          <p class="main-text">
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut cum
+            aliquid
+          </p>
+          <p class="date text-[12.5px] font-bold">2025</p>
+        </div>
         <div
           class="card w-[225px] h-[225px] bg-amber-300 p-[10px] rounded-[15px] flex flex-col justify-between mr-[20px] mb-[20px]"
         >
